387643d5c3e37a517632d52f78ac321e0dc37916
Loic Messy <loiccarrel.messy@gmail.com>
2024-09-18 11:26:47 +0200

    initialize kafka consumer

---
+++ src/main/java/com/komastech/kafka_consumer_ex/KafkaConsumerExApplication.java
@@ -0,0 +1,13 @@
+package com.komastech.kafka_consumer_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/komastech/kafka_consumer_ex/KafkaConsumerExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komastech.kafka_consumer_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
